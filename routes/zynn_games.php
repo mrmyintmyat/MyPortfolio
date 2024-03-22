c611682b8998b952d3e5c7057d51ef6c980ff441
Myint Myat Aung <myam6552@gmail.com>
2024-03-22 13:23:51 +0630

    new

--- routes/zynn_games.php
+++ routes/zynn_games.php
@@ -13,17 +13,21 @@ use App\Http\Controllers\Admin\GameController as AdminGameController;
 Auth::routes([
     'verify' => true,
 ]);
-// Route::post('/logout', function () {
-//     Auth::user()->update(
-//         [
-//          'is_logged_in' => false,
-//          'device_token' => null,
-//         ]
-//     );
-//     Auth::logout();
+Route::post('/logout', function () {
+    Auth::user()->update(
+        [
+         'is_logged_in' => false,
+         'device_token' => null,
+        ]
+    );
+    Auth::logout();
 
-//     return redirect('/');
-// })->name('logout');
+    $request->session()->invalidate();
+
+    $request->session()->regenerateToken();
+
+    return redirect('/');
+})->name('logout');
 //admin panel
 Route::group(['middleware' => ['auth', 'checkadmin']], function () {
     // Route::get('/admin/panel/games/create', [AdminController::class, 'post_game']);
