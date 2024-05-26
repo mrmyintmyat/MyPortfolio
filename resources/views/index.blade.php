ec04f47903b9af62f1140bea9c4e9192c81f1726
Myint Myat Aung <myam6552@gmail.com>
2024-05-26 17:18:11 +0630

    MyPortfolio

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -549,7 +549,7 @@
                 console.log("OKK")
 
                 $.ajax({
-                    url: "{{ route('send_message') }}",
+                    url: "{{ secure_url('send_message') }}",
                     type: "POST",
                     data: formData,
                     success: function(response) {
