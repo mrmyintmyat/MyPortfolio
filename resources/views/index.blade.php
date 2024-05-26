f32239f322901739bec3e3542b723d4a1d1ea587
Myint Myat Aung <myam6552@gmail.com>
2024-05-26 16:37:14 +0630

    MyPortfolio

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -501,7 +501,6 @@
     </div>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    {{-- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
